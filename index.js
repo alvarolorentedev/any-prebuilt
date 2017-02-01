ca5bc2a9bc0d5a90f5f8fa208865540e8592061a
alvaro <Alvarojosepl@gmail.com>
2017-02-01 07:00:51 -0600

    solved export issues

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var findProjectRoot = require('find-project-root'),
+    path = require('path')
+
+function getPlatformInfo() {
+    if (/linux/.test(process.platform)) {
+        return process.arch == 32 ? 'linux:ia32' : 'linux:x64'
+    } else if (/darwin/.test(process.platform)) {
+        return 'osx:' + process.arch
+    } else {
+        return 'windows:' + process.arch
+    }
+}
+
 try {
     var rootdir = findProjectRoot(process.cwd(), { maxDepth: 12 })
     var manifest = require(path.join(rootdir, 'package.json'))
@@ -5,10 +18,11 @@ try {
     var targetDir = process.env.PREBUILT_TARGET_DIR || inf.targetDir || './bin'
     var targetBin = process.env.PREBUILT_BINARY || inf.targetBin
     var paths = inf.paths
-} catch (e) {}
+    var platform = process.env.PREBUILT_PLATFORM || inf.platform || getPlatformInfo().split(':')[0]
+} catch (e) {console.log(e)}
 
 if (targetBin)
-    module.exports = require(targetDir + '/' + targetBin);
+    module.exports.binary = require(targetDir + '/' + targetBin);
 
 if(paths)
-    module.exports.paths = paths
+    module.exports.path = path.join(__dirname, paths[platform])
