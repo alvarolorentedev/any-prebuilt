d0391b237ee4bf16240ec529d261e0e1bb612286
kanekotic <alvarojosepl@gmail.com>
2018-07-14 10:09:29 +0200

    implement file retriever

--- test/lib/helpers/retriever-test.js
+++ test/lib/helpers/retriever-test.js
@@ -1,17 +1,34 @@
-jest.mock('request', () => {
-    return jest.fn()
+jest.mock('request-promise-native', () => {
+    let expectedFun = jest.fn()
+    return {
+        get: jest.fn(() => {
+            return {
+                on: expectedFun
+            }
+        })
+    }
 })
 const retriever = require('../../../lib/helpers/retriever'),
     faker = require('faker')
-    request = require('request')
+    request = require('request-promise-native')
 
 describe('retriever', () => {
+    let onMock = jest.fn()
+
+    beforeEach(() => {
+        request.get.mockImplementation(() => {
+            return {
+                on: onMock
+            }
+        })
+    })
+
+    let defaultProgress = () => {}
     test('retriever calls get with correct settings with token for private repos', async () => {
         let url = faker.internet.url(),
             token = faker.internet.url(),
             settings = { 
             url: url, 
-            method: 'GET',
             headers: {
                 'User-Agent': 'request',
                 Accept: 'application/octet-stream'
@@ -21,21 +38,50 @@ describe('retriever', () => {
                 sendImmediately: true
             }
         }
-        await retriever(url, token)
-        expect(request).toBeCalledWith(settings)
+        await retriever(url, defaultProgress, token)
+        await retriever(url, defaultProgress, token)
+        expect(request.get).toBeCalledWith(settings)
     })
 
     test('retriever calls get with correct settings without token for public repos', async () => {
         let url = faker.internet.url(),
             settings = { 
             url: url, 
-            method: 'GET',
             headers: {
                 'User-Agent': 'request',
                 Accept: 'application/octet-stream'
             }
         }
-        await retriever(settings.url)
-        expect(request).toBeCalledWith(settings)
+        await retriever(url)
+        expect(request.get).toBeCalledWith(settings)
+    })
+
+    test('retrieve returns result from request', async () => {
+        let url = faker.internet.url()
+        let expectResult = {on: () =>{}, a : faker.random.uuid()}
+        request.get.mockReturnValue(expectResult)
+
+        let result = await retriever(url)
+
+        expect(result).toEqual(expectResult)
+
+    })
+
+    test('retrieve track progress', async () => {
+        let url = faker.internet.url(),
+            chunk = { length: faker.random.number()},
+            progress = jest.fn()
+
+        onMock.mockImplementation((type,cb) => {
+            if(type == 'data')
+                cb(chunk)
+            else
+                throw "here be dragons"
+        })
+
+        await retriever(url,progress)
+
+        expect(progress).toBeCalledWith(chunk.length)
+
     })
 })
