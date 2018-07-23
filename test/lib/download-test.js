d8f23fa35245df9708ecafe49a37747beec5721f
kanekotic <alvarojosepl@gmail.com>
2018-07-23 21:15:09 +0300

    binded code for download and unpack

--- test/lib/download-test.js
+++ test/lib/download-test.js
@@ -1,7 +1,32 @@
-const download = require('../../lib/download')
+jest.mock('../../lib/retriever', () => jest.fn())
+jest.mock('../../lib/unpack/unpacker', () => jest.fn())
+jest.mock('path', () => ({
+    extname: jest.fn()
+}))
+
+const download = require('../../lib/download'),
+    retriever = require('../../lib/retriever'),
+    unpacker = require('../../lib/unpack/unpacker'),
+    path = require('path'),
+    faker = require('faker')
 
 describe('download', () => {
-    test('download should request file from remote', async () => {
-        expect(download).not.toBeUndefined()
+    test('should retrieve file and unpack', async () => {
+        let url = faker.random.uuid(), 
+            progressTracker = faker.random.uuid(),
+            token = faker.random.uuid(),
+            name = faker.random.uuid(),
+            destination = faker.random.uuid(),
+            extname = faker.random.uuid(),
+            retrieverResult = faker.random.uuid()
+
+        retriever.mockReturnValue(Promise.resolve(retrieverResult))
+        path.extname.mockReturnValue(extname)
+
+        await download(destination, name, url, progressTracker, token)
+
+        expect(retriever).toBeCalledWith(url, progressTracker, token)
+        expect(path.extname).toBeCalledWith(name)
+        expect(unpacker).toBeCalledWith(destination, retrieverResult, extname)
     })
 })
