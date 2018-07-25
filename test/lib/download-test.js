ff39c83f6757156b2319910d5398fa62b23be20e
kanekotic <alvarojosepl@gmail.com>
2018-07-25 23:12:49 +0200

    binded download bar track

--- test/lib/download-test.js
+++ test/lib/download-test.js
@@ -1,5 +1,6 @@
 jest.mock('../../lib/retriever', () => jest.fn())
 jest.mock('../../lib/unpack/unpacker', () => jest.fn())
+jest.mock('../../lib/helpers/progress-tracker', () => jest.fn())
 jest.mock('path', () => ({
     extname: jest.fn()
 }))
@@ -7,25 +8,26 @@ jest.mock('path', () => ({
 const download = require('../../lib/download'),
     retriever = require('../../lib/retriever'),
     unpacker = require('../../lib/unpack/unpacker'),
+    tracker = require('../../lib/helpers/progress-tracker'),
     path = require('path'),
     faker = require('faker')
 
 describe('download', () => {
     test('should retrieve file and unpack', async () => {
         let url = faker.random.uuid(), 
-            progressTracker = faker.random.uuid(),
             token = faker.random.uuid(),
             name = faker.random.uuid(),
             destination = faker.random.uuid(),
             extname = faker.random.uuid(),
             retrieverResult = faker.random.uuid()
 
-        retriever.mockReturnValue(Promise.resolve(retrieverResult))
+        retriever.mockReturnValue(retrieverResult)
         path.extname.mockReturnValue(extname)
 
-        await download(destination, name, url, progressTracker, token)
+        await download(destination, name, url, token)
 
-        expect(retriever).toBeCalledWith(url, progressTracker, token)
+        expect(retriever).toBeCalledWith(url, token)
+        expect(tracker).toBeCalledWith(retrieverResult)
         expect(path.extname).toBeCalledWith(name)
         expect(unpacker).toBeCalledWith(destination, retrieverResult, extname)
     })
