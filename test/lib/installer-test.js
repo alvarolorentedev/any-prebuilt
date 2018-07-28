f79dfbe55ed2dbec7bbb0a68f7a35fbd4f22347c
kanekotic <alvarojosepl@gmail.com>
2018-07-28 08:52:09 +0200

    star integrating installer calls

--- test/lib/installer-test.js
+++ test/lib/installer-test.js
@@ -1,7 +1,26 @@
-const installer = require('../../lib/installer')
+jest.mock('../../lib/helpers/platform', () => ({
+    map: jest.fn(), 
+    validate: jest.fn()
+}))
+jest.mock('../../lib/retriever', () => ({
+    getReleaseInfo: jest.fn()
+}))
+const installer = require('../../lib/installer'),
+    platform = require('../../lib/helpers/platform'),
+    retriever = require('../../lib/retriever'),
+    faker = require('faker')
 
 describe('index', () => {
     test('exports install', async () => {
-        expect(installer).not.toBeUndefined()
+        let manifest = { something: faker.random.uuid() },
+            mappedManifest = { something: faker.random.uuid() }
+
+        platform.map.mockReturnValue(mappedManifest)
+        
+        installer(manifest)
+        
+        expect(platform.map).toBeCalledWith(manifest)
+        expect(platform.validate).toBeCalledWith(mappedManifest)
+        expect(retriever.getReleaseInfo).toBeCalledWith(mappedManifest)
     })
 })
