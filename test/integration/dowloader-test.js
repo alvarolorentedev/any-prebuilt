ca418b98a1ccd2660d84d889f58674bf3b7785fb
kanekotic <me@kanekotic.com>
2022-07-28 10:40:27 +0200

    Alvaro | fix tests

--- test/integration/dowloader-test.js
+++ test/integration/dowloader-test.js
@@ -1,17 +1,19 @@
 const download = require('../../lib/download')
-var fs = require('fs');
+var fs = require('fs')
+var path = require('path')
+
 describe('download should', () => {
 
     test('uncompress zip file in correct path', async () => {
-        await download('./temp', 'test.zip', 'https://github.com/kanekotic/any-prebuilt-test/releases/download/0.0.1/test_osx_x64.zip')
-        expect(fs.existsSync("./temp/test/hello.txt")).toBeTruthy()
-        fs.unlinkSync("./temp/test/hello.txt")
+        await download(path.join(__dirname,'temp'), 'test.zip', 'https://github.com/kanekotic/any-prebuilt-test/releases/download/0.0.1/test_osx_x64.zip')
+        expect(fs.existsSync(path.join(__dirname,'temp', 'test', 'hello.txt'))).toBeTruthy()
+        fs.unlinkSync(path.join(__dirname,'temp', 'test', 'hello.txt'))
     })
     
     test('uncompress tgz file in correct path', async () => {
-        await download('./temp', 'test.tgz', 'https://github.com/kanekotic/any-prebuilt-test/releases/download/0.0.1/test_osx_x64.tgz')
-        expect(fs.existsSync("./temp/test/hello.txt")).toBeTruthy()
-        fs.unlinkSync("./temp/test/hello.txt")
+        await download(path.join(__dirname,'temp'), 'test.tgz', 'https://github.com/kanekotic/any-prebuilt-test/releases/download/0.0.1/test_osx_x64.tgz')
+        expect(fs.existsSync(path.join(__dirname,'temp', 'test', 'hello.txt'))).toBeTruthy()
+        fs.unlinkSync(path.join(__dirname,'temp', 'test', 'hello.txt'))
     })
     
 })
--- test/integration/installer-test.js
+++ test/integration/installer-test.js
@@ -1,20 +1,22 @@
 const installer = require('../../lib/installer')
-var fs = require('fs');
+var fs = require('fs')
+var path = require('path')
+
 describe('download should', () => {
 
     test('install latest version', async () => {
         let manifest = {
             "arch": "x64",
             "platform": 'osx',
-            "targetDir": "./temp",
+            "targetDir": path.join(__dirname,'temp'),
             "version": "latest",
             "user": "kanekotic",
             "repo": "any-prebuilt-test",
             "token": process.env.GITHUB_TEST_TOKEN
           }
         await installer(manifest)
-        expect(fs.existsSync("./temp/test/hello.txt")).toBeTruthy()
-        fs.unlinkSync("./temp/test/hello.txt")
+        expect(fs.existsSync(path.join(__dirname,'temp', 'test', 'hello.txt'))).toBeTruthy()
+        fs.unlinkSync(path.join(__dirname,'temp', 'test', 'hello.txt'))
     })
     
 })
